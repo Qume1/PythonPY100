cbe5032531103c6a638f98727d0bbd92490eb0a9
Qume1 <semyon.sirotin@2013@yandex.ru>
2022-03-23 20:38:08 +0300

    init

--- Занятие2/Домашнее задание/task3/main.py
+++ Занятие2/Домашнее задание/task3/main.py
@@ -1,5 +1,10 @@
 A = int(input())
 B = int(input())
 C = int(input())
-
-if
+# A_cond = A < 45 and B >= 45 and C >= 0
+# B_cond = A >= 45 and B < 45 and C >= 45
+# C_cond = A >= 45 and B >= 45 and C < 45
+if (A < 45 and B >= 45 and C >= 0) or (A >= 45 and B < 45 and C >= 45) or (A >= 45 and B >= 45 and C < 45):
+    print(True)
+else:
+    print(False)
