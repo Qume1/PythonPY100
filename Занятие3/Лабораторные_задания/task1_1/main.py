7ed74f7c4bcbdbcfeccc9c39619e09bf05457f82
Qume1 <semyon.sirotin@2013@yandex.ru>
2022-03-30 20:34:11 +0300

    init

--- Занятие3/Лабораторные_задания/task1_1/main.py
+++ Занятие3/Лабораторные_задания/task1_1/main.py
@@ -3,9 +3,7 @@ def main():
     current_sum = 0  # текущая сумма
     max_sum = 500  # максимальная сумма
     while True:
-        if current_sum > max_sum:
-            current_sum = current_sum - n ** 2
-            n -= 1
+        if current_sum + n ** 2 > max_sum:
             break
         if current_sum < max_sum:
             current_sum = current_sum + n ** 2
