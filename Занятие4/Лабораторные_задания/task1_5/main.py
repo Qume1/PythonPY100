18588ed24d37cd37f4cc7bdecb0505553fd0f1af
Qume1 <semyon.sirotin@2013@yandex.ru>
2022-04-06 19:53:32 +0300

    init

--- Занятие4/Лабораторные_задания/task1_5/main.py
+++ Занятие4/Лабораторные_задания/task1_5/main.py
@@ -1,7 +1,17 @@
+def check(n: int, m: int) -> str:
+    if n > m:
+        return "Четных больше"
+    elif n < m:  # TODO распечатать каких чисел больше. Учтите, что длины могу быть равны
+        return "Нечетных больше"
+    else:
+        return "Кол-во чисел равно"
+
 if __name__ == "__main__":
     list_ = [4, -1, 10, -1, 3, -3, -6, 8, 6, 9]
-    ...  # TODO получить два списка четных и нечетных чисел
+    even_list_ = [i for i in list_ if i % 2 == 0]  # TODO получить два списка четных и нечетных чисел
+    not_even_list_ = [i for i in list_ if i % 2 != 0]
 
-    ...  # TODO найти длины этих списков
+    len_even_list_ = len(even_list_)  # TODO найти длины этих списков
+    len_not_even_list_ = len(not_even_list_)
 
-    ...  # TODO распечатать каких чисел больше. Учтите, что длины могу быть равны
+    check(len_even_list_, len_not_even_list_)
